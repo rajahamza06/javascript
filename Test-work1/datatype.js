83ccd9a83f60484d8141e31a03676ba16cd730a1
Hamza Gul <149502689+rajahamza06@users.noreply.github.com>
2024-04-18 18:23:11 +0000

    data types

---
+++ Test-work1/datatype.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) // we are using node.js not browser
+
+let name = "raja"
+let age = 22
+let isLoggedIn = false
+let state;
+
+/*----------------------Data types--------------*/
+// number => 2 to power 53
+// bignit
+// string => ""
+// boolean => true/false
+// null
+// undefined=>
+// symbol => for uniqueness
+
+/*------------Objects-------------*/
+
+
+
+
+
+
+
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
