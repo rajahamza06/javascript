4218ad4b3ea1ec163639827a0f633363b2c8749a
Hamza Gul <149502689+rajahamza06@users.noreply.github.com>
2024-04-19 14:24:45 +0000

    changes in operations

--- Test-work1/operation.js
+++ Test-work1/operation.js
@@ -1,11 +1,11 @@
 let score = "33abs"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 /* ----------------------------------*/
 // number "33" or any number easily convewrt to number like =>33
@@ -17,10 +17,39 @@ console.log(valueInNumber);
 
 let isLoggedIn = "name"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 
 
 let someNumber = 25
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+//========================== OPERATONS =====================//
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+let str1 = "raja"
+let str2 = "gul"
+let str3 = str1 + str2
+
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 2 + "2");
+// console.log(1 + 2 * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
