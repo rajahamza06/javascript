666c2f9006dd2fe69ff4dc814a7702efa7fedb04
Hamza Gul <149502689+rajahamza06@users.noreply.github.com>
2024-04-22 19:50:31 +0000

    numbers and math in java script

---
+++ datatypes/numbers.js
@@ -0,0 +1,31 @@
+const score = 400
+
+const balance = new Number(100)
+// console.log(balance); 
+// console.log(score); 
+// console.log(balance.toFixed(2));
+
+const otherNmuber = 123.685
+// console.log(otherNmuber.toPrecision(3));
+
+const secNumber = 1000000
+// console.log(secNumber.toLocaleString( ));
+
+//---------------------MATHS------------------// 
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6)); 
+// console.log(Math.ceil(4.2)); 
+// console.log(Math.floor(4.9)); 
+// console.log(Math.min(4,3,5,6)); 
+// console.log(Math.max(4,3,5,6)); 
+
+
+console.log(Math.random());
+console.log(Math.random()*10 + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);   
