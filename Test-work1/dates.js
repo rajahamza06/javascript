67175929156013f65b25810dc60c3c6690499880
Hamza Gul <149502689+rajahamza06@users.noreply.github.com>
2024-04-23 13:39:36 +0000

    new datye

---
+++ Test-work1/dates.js
@@ -0,0 +1,9 @@
+// dates
+
+let mydate = new Date()
+console.log(typeof(mydate));
+console.log(mydate.toDateString());
+console.log(mydate.toISOString());
+console.log(mydate.toJSON());
+console.log(mydate.toLocaleDateString());
+console.log(mydate.toLocaleTimeString());
