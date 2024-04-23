2c02a9d0d5ed4ff3860d5f82ed19b1262cadd4e2
Hamza Gul <149502689+rajahamza06@users.noreply.github.com>
2024-04-23 17:29:08 +0000

    array using in javascript

---
+++ Test-work2/array.js
@@ -0,0 +1,33 @@
+const arrayone = [0,1,2,3,4,5]
+// const arraytwo = ["hui", "hur", "huf"]
+
+const arraytwo = new Array(1,2,3,4)
+console.log(arraytwo[1]);
+
+//-------------------------ARRAY METHOD-----------------
+
+// arraytwo.push(6)
+// arraytwo.push(7)
+
+// arraytwo.unshift(0)
+
+
+// console.log(arraytwo.includes(9));
+// console.log(arraytwo.indexOf(2));
+
+const newarr = arraytwo.join()
+
+
+console.log(arraytwo);
+console.log(newarr);
+
+
+// --------SLICE
+console.log("A", arraytwo);
+const myn1 = arraytwo.slice(1, 3)
+console.log(myn1);
+console.log("B", arraytwo);
+
+const myn2 = arraytwo.splice(1, 3)
+console.log("c", arraytwo);
+console.log(myn2);
